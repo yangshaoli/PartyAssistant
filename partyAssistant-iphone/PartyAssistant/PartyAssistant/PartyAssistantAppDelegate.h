616b6f687a093e2c2638803b9afac4da1387553a
Li Chao <lichao0708@gmail.com>
2011-10-26 09:35:43 +0800

    make iphone

---
+++ partyAssistant-iphone/PartyAssistant/PartyAssistant/PartyAssistantAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PartyAssistantAppDelegate.h
+//  PartyAssistant
+//
+//  Created by 超 李 on 11-10-26.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartyAssistantAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ partyAssistant-iphone/PartyAssistant/PartyAssistant/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  PartyAssistant
+//
+//  Created by 超 李 on 11-10-26.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
