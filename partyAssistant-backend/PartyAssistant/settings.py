f004bcd0c86d2ddd26eac7f7563259b0b1631c86
perchouli <jp.chenyang@gmail.com>
2011-11-01 18:48:41 +0800

    Fix conflict

--- partyAssistant-backend/PartyAssistant/settings.py
+++ partyAssistant-backend/PartyAssistant/settings.py
@@ -54,11 +54,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-<<<<<<< HEAD
-MEDIA_ROOT = '%(PROJECT_ROOT)s/media/' % locals()
-=======
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
->>>>>>> liwenjian
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
