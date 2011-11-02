03c61d8ab2ec14f952e13320d25202850a595f89
LiuXue <hljxuege@foxmail.com>
2011-11-02 15:25:30 +0800

    Change parties

---
+++ partyAssistant-backend/PartyAssistant/parties/models.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+'''
+Created on 2011-10-27
+
+@author: liuxue
+'''
+from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+class Party(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(blank = True)
+    address = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+    creator = models.ForeignKey(User)
+    limit_num = models.IntegerField(max_length=3)
+    def __unicode__(self):
+        return self.id
+    
+    def get_apply_url(self):
+        return reverse('event_apply', args=[self.id])
+    
