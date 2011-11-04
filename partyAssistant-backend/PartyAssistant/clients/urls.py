7f3d39e1c62b7cfc7f6ce6964062ace20dc71dfc
LiuXue <hljxuege@foxmail.com>
2011-11-04 10:22:15 +0800

    741 and 754 .add apply_list and notapplylist not_esponselist

--- partyAssistant-backend/PartyAssistant/clients/urls.py
+++ partyAssistant-backend/PartyAssistant/clients/urls.py
@@ -13,7 +13,10 @@ urlpatterns = patterns('',
     # url(r'^admb.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^apply2not/(?P<linkman_id>\d+)/$','clients.views.apply2not',name='apply2not'),
     url(r'^change_apply_status/$','clients.views.change_apply_status',name='change_apply_status'),
     url(r'^invite_list/(?P<party_id>\d+)/$','clients.views.invite_list',name='invite_list'),
+    url(r'^apply_list/(?P<party_id>\d+)/$','clients.views.apply_list',name='apply_list'),
+    url(r'^notresponse_list/(?P<party_id>\d+)/$','clients.views.notresponse_list',name='notresponse_list'),
+    url(r'^notapply_list/(?P<party_id>\d+)/$','clients.views.notapply_list',name='notapply_list'),
+
     )
--- partyAssistant-backend/PartyAssistant/clients/views.py
+++ partyAssistant-backend/PartyAssistant/clients/views.py
@@ -7,19 +7,10 @@ Created on 2011-11-2
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import get_object_or_404,render_to_response
 from django.template import RequestContext
-from models import Client
 from models import Client_Party
 from parties.models import Party
 from django.http import HttpResponse
 #更改报名状态 url:/(?P<linkman_id>\d+)/$
-#报名-->不报名
-def apply2not(request,linkman_id):
-    if request.method == 'POST':
-        party_id = request.party_id #request 存放当前 的Party
-        linkman_party_l=get_object_or_404(Client_Party,linkman_id=linkman_id,party_id=party_id)  
-        linkman_party=linkman_party_l[0]
-        linkman_party.apply_status=u'未报名'
-    return 'ok'
 
 @csrf_protect
 def change_apply_status(request):
@@ -30,15 +21,7 @@ def change_apply_status(request):
         print client_party.apply_status
     return HttpResponse("OK")
 
-#不报名-->报名
-def not2apply(request,linkman_id):
-    pass
-#未响应-->报名
-def noresponse2apply(request,linkman_id):
-    pass
-#未响应-->不报名
-def noresponse2not(request,linkman_id):
-    pass
+
 #受邀人员列表
 def invite_list(request,party_id):
 #    if request.method == 'POST':
@@ -47,12 +30,20 @@ def invite_list(request,party_id):
     return render_to_response('clients/invite_list.html',{'client_party_list':client_party_list}, context_instance=RequestContext(request)) 
 
 #报名人员列表
-def apply_list(request):
-    pass
+def apply_list(request,party_id):
+    party = get_object_or_404(Party, pk=1)
+    client_party_list=Client_Party.objects.filter(party=party,apply_status=u'报名')
+    return render_to_response('clients/invite_list.html',{'client_party_list':client_party_list}, context_instance=RequestContext(request)) 
+
 #未向应人员列表
-def notresponse_list(request):
-    pass
+def notresponse_list(request,party_id):
+    party = get_object_or_404(Party, pk=1)
+    client_party_list=Client_Party.objects.filter(party=party,apply_status=u'未响应')
+    return render_to_response('clients/invite_list.html',{'client_party_list':client_party_list}, context_instance=RequestContext(request)) 
+
 #未报名人员列表
-def notapply(request):
-    pass
+def notapply_list(request,party_id):
+    party = get_object_or_404(Party, pk=1)
+    client_party_list=Client_Party.objects.filter(party=party,apply_status=u'未报名')
+    return render_to_response('clients/invite_list.html',{'client_party_list':client_party_list}, context_instance=RequestContext(request)) 
 
