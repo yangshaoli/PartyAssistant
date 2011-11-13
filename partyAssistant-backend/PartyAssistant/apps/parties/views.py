051e1a254918646840752dfa07972b3aae0d400b
perchouli <jp.chenyang@gmail.com>
2011-11-13 20:16:13 +0800

    Send invite checkout=True

--- partyAssistant-backend/PartyAssistant/apps/parties/views.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/views.py
@@ -18,7 +18,7 @@ from forms import CreatePartyForm, InviteForm
 from models import Party
 from settings import SYS_EMAIL_ADDRESS, DOMAIN_NAME
 from utils.tools.email_tool import send_emails
-import datetime, copy
+import copy
 
 
 
@@ -142,7 +142,8 @@ def email_invite(request, party_id):
             content = EmailMessage.objects.get(party=party).content
             data = {
             'client_email_list': client_email_list, 
-            'content': content
+            'content': content,
+            'is_apply_tips' : True
         }
             form = EmailInviteForm(data)
         else:
@@ -242,80 +243,12 @@ def copy_party(request,party_id):#复制party和联系人
     for client in clients:
         c = PartiesClients.objects.create(client_id=client.id, party=new_party,apply_status='noanwser')
         c.save()
-    
-    
-    return render_to_response('parties/edit_party.html',{'form':CreatePartyForm(instance=new_party), 'party':new_party},context_instance=RequestContext(request))
-#
-#    if request.method == 'GET':
-#        old_party = Party.objects.get(pk=int(party_id))        
-#        date = datetime.datetime.strftime(old_party.time,'%Y-%m-%d')
-#        time = datetime.datetime.strftime(old_party.time,'%H:%M:%S')
-#        return render_to_response('parties/copy_party.html',{'old_party':old_party,'date':date,'time':time,'form':CreatePartyForm()},context_instance=RequestContext(request))
-#    else :
-#        old_party = Party.objects.get(pk=int(party_id))
-#        form = CreatePartyForm(request.POST)
-#        if form.is_valid():        
-#            time = form.cleaned_data['time']
-#            address=form.cleaned_data['address']
-#            description=form.cleaned_data['description']  
-#            limit_num = form.cleaned_data['limit_num']   
-#            new_party=Party.objects.create(
-#                           time=time,
-#                           address=address,
-#                           description=description,                           
-#                           creator=request.user,
-#                           limit_num=limit_num                                  
-#                           );
-#            #复制联系人
-#            client_party_list = PartiesClients.objects.filter(party=old_party) 
-#            for client_party in client_party_list:
-#                PartiesClients.objects.create(
-#                                            client =client_party.client,
-#                                            party=new_party,
-#                                            apply_status=u'被邀请'
-#                                            )       
-#            return list_party(request)
-#        else:
-#            return render_to_response('parties/copy_party.html',{'form':form,'old_party':old_party}, context_instance=RequestContext(request)) 
-    
 
-'''
-@summary: 处理短信邀请和邮件邀请
-@author: chenyang
-'''
+    return render_to_response('parties/edit_party.html',{'form':CreatePartyForm(instance=new_party), 'party':new_party},context_instance=RequestContext(request))
+
 def message_invite(request):
     form = InviteForm()
     return render_to_response('parties/invite.html',{'form':form, 'title':u'发送短信通知'}, context_instance=RequestContext(request))
-
-#def email_invite(request, party_id):
-#    email_subject = u'[PartyAssistant]您收到一个活动邀请'
-#    party = Party.objects.get(pk=party_id)
-#    if request.method=='POST':
-#        form = InviteForm(request.POST)
-#        if form.is_valid():
-#            addressees = form.cleaned_data['addressee']
-#            content = form.cleaned_data['content']
-#            for addressee in addressees.split(','):
-#                if addressee:
-#                #如果带报名提示，则内容中带上报名链接
-#                    if request.POST['enroll_link']:
-#                        enroll_link = DOMAIN_NAME+'/clients/invite_enroll/'+addressee+'/'+party_id
-#                        content = content + u'点击进入报名页面：<a href="%s">%s</a>' % (enroll_link, enroll_link)
-#                    send_emails(email_subject, content, SYS_EMAIL_ADDRESS, [addressee])
-#                    #将收件人加入clients,状态为'未报名'
-#                    if Client.objects.filter(email=addressee, creator=User.objects.get(pk=request.user.id)).count() == 0:
-#                        client = Client.objects.create(email=addressee, creator=User.objects.get(pk=request.user.id), invite_type='email')
-#                        PartiesClients.objects.create(client=client, party=Party.objects.get(pk=party_id), apply_status=u'未报名')
-#            return render_to_response('message.html', context_instance=RequestContext(request))
-#    else:
-#        form = InviteForm()
-#        ctx = {
-#            'form':form,
-#            'party':party,
-#            'invite_num':Client.objects.filter(invite_type='email').count(),
-#            'title':u'发送邮件通知'
-#        }
-#        return render_to_response('parties/invite.html', ctx, context_instance=RequestContext(request))
 
 @login_required
 def list_party(request):
@@ -334,4 +267,5 @@ def view_party(request, party_id):
         'party' : party,
         'client': client,
     }
+    
     return render_to_response('parties/show.html', ctx ,context_instance=RequestContext(request))
