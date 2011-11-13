f441bb9d6acbe18e9ac724836b94dff5c54e3df5
perchouli <jp.chenyang@gmail.com>
2011-11-13 15:51:25 +0800

    Merge liwenjian

--- partyAssistant-backend/PartyAssistant/apps/messages/admin.py
+++
@@ -1,6 +0,0 @@
-from apps.messages.models import EmailMessage, SMSMessage, BaseMessage
-from django.contrib import admin
-
-admin.site.register(EmailMessage)
-admin.site.register(SMSMessage)
-admin.site.register(BaseMessage)
