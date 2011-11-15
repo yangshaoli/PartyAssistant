363f3cf7fb4902d4d2ec0117f957ec8aeeb7a459
LiuXue <hljxuege@foxmail.com>
2011-11-15 17:56:40 +0800

    #835 limit_count maxlength=3

--- partyAssistant-backend/PartyAssistant/apps/parties/forms.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/forms.py
@@ -3,12 +3,15 @@
 from apps.parties.models import Party
 from django import forms
 from django.core.validators import validate_email
+from django.forms.widgets import TextInput
 
 class CreatePartyForm(forms.ModelForm):
     class Meta:
         model = Party
         fields = ('start_time', 'address', 'description', 'limit_count')
-
+        widgets = {
+            'limit_count': TextInput(attrs={'maxlength':'3'}),
+        }
 class InviteForm(forms.Form):
     addressee = forms.CharField(widget=forms.TextInput(), required=True)
-    content = forms.CharField(widget=forms.TextInput(), required=True)
+    content = forms.CharField(widget=forms.TextInput(), required=True)   
