d64088eb73a751e789ff7e10b0d9c6fbc8b7d0ed
liwenjian <liwenjian@cn-acg.com>
2011-11-15 11:34:28 +0800

    add log config

--- partyAssistant-backend/PartyAssistant/settings.py
+++ partyAssistant-backend/PartyAssistant/settings.py
@@ -141,20 +141,54 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
+    'version': 1, 
+    'disable_existing_loggers': True, 
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        }, 
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
+        'null': {
+            'level': 'DEBUG', 
+            'class': 'django.utils.log.NullHandler',
+        }, 
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'level': 'DEBUG', 
+            'class': 'logging.handlers.RotatingFileHandler', 
+            'formatter': 'verbose', 
+            'filename': 'airenao.log', 
+            'maxBytes': 1048576, 
+            'backupCount': 10
+        }, 
         'mail_admins': {
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
         }
     },
     'loggers': {
+        'django': {
+            'handlers':['null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
-            'propagate': True,
+            'propagate': False,
         },
+        'airenao': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        }
     }
 }
 
