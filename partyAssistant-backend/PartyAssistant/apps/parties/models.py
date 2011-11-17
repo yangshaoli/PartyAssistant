b4e5730277097f16ccccc997ab140edbfc6793d6
LiuXue <hljxuege@foxmail.com>
2011-11-17 11:10:13 +0800

    PartiesClients add is_see_over

--- partyAssistant-backend/PartyAssistant/apps/parties/models.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/models.py
@@ -41,3 +41,4 @@ class PartiesClients(models.Model):
     client = models.ForeignKey(Client)
     party = models.ForeignKey(Party)
     apply_status = models.CharField(max_length=16, choices=APPLY_STATUS, default='noanswer')
+    is_see_over = models.BooleanField(default=False)
