38979a444269aef6660849e7a7d8daa24764b59a
LiuXue <hljxuege@foxmail.com>
2011-11-18 10:45:45 +0800

    modify copy

--- partyAssistant-backend/PartyAssistant/apps/parties/views.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/views.py
@@ -20,7 +20,8 @@ from settings import SYS_EMAIL_ADDRESS, DOMAIN_NAME
 from utils.tools.email_tool import send_emails
 import copy
 import datetime
-
+import logging
+logger = logging.getLogger('airenao')
 
 
 @login_required
@@ -298,22 +299,33 @@ def delete_party_notice(request,party_id):
 def copy_party(request,party_id):#复制party和联系人
     old_party = Party.objects.get(pk=party_id)
     if request.method == 'GET':        
-        new_party = copy.deepcopy(old_party)
-        new_party.id = None
+        new_party = Party(creator=old_party.creator,
+                          start_time=old_party.start_time,
+                          address=old_party.address,
+                          description=old_party.description,
+                          limit_count=old_party.limit_count,
+                          invite_type=old_party.invite_type
+                          )
         new_party.save()
         old_message = None
         if old_party.invite_type:
             try:
                 if old_party.invite_type == 'email':
                     old_message = get_object_or_404(EmailMessage, party=old_party)
+                    new_message = EmailMessage(subject=old_message.subject,
+                                               content=old_message.content,
+                                               party=new_party                                               
+                                               )
                 if old_party.invite_type == 'phone':    
                     old_message = get_object_or_404(SMSMessage, party=old_party)
-            except:
-                pass
+                    new_message = SMSMessage(content=old_message.content,
+                                             party=new_party                                               
+                                             )
+            except Exception, ex: 
+                new_e = Exception()
+                new_e.error_msg = str(ex)
+                logger.error('Email send')
             else:
-                new_message = copy.deepcopy(old_message)
-                new_message.id = None
-                new_message.party = new_party
                 new_message.save()
         
         clients = PartiesClients.objects.filter(party=party_id).exclude(client__invite_type='public')
