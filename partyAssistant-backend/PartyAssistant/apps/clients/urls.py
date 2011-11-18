f2f129dd73d03d05b77d4856e9d4b808bd17482a
perchouli <jp.chenyang@gmail.com>
2011-11-18 11:36:21 +0800

    Moveing enroll page from clients to parties

--- partyAssistant-backend/PartyAssistant/apps/clients/urls.py
+++ partyAssistant-backend/PartyAssistant/apps/clients/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('apps.clients.views',
-    url(r'^public_enroll/(?P<party_id>\d+)/$', 'public_enroll', name = 'public_enroll'),
-    url(r'^invite_enroll/(?P<email>\S+)/(?P<party_id>\d+)/$', 'invite_enroll', name = 'invite_enroll'),
+
     url(r'^change_apply_status/$','change_apply_status',name='change_status'),
     url(r'^invite_list/(?P<party_id>\d+)/$','invite_list',name='invite_list'),
 
