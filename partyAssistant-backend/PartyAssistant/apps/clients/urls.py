80ede060275257e68d6430e331a325c9874c1be7
LiuXue <hljxuege@foxmail.com>
2011-11-18 15:47:56 +0800

    add ajax

--- partyAssistant-backend/PartyAssistant/apps/clients/urls.py
+++ partyAssistant-backend/PartyAssistant/apps/clients/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('apps.clients.views',
 
     url(r'^change_apply_status/$','change_apply_status',name='change_status'),
     url(r'^invite_list/(?P<party_id>\d+)/$','invite_list',name='invite_list'),
-
+    url(r'^invite_list_ajax/(?P<party_id>\d+)/$','invite_list_ajax',name='invite_list_ajax'),
 )
--- partyAssistant-backend/PartyAssistant/apps/clients/views.py
+++ partyAssistant-backend/PartyAssistant/apps/clients/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import RequestContext
 from django.template.response import TemplateResponse
+from django.utils import simplejson
 
 #获得报名/未相应/不参加的客户数
 def get_client_sum(party_id):
@@ -74,3 +75,39 @@ def invite_list(request, party_id):
     return TemplateResponse(request,'clients/invite_list.html',ctx) 
 
 
+def invite_list_ajax(request, party_id):
+    apply_status = request.GET.get('apply', 'all')
+    party = Party.objects.get(id=party_id)
+    party_clients_list = []
+    party_clients_list_ajax = []
+    if apply_status == 'all':
+        party_clients_list = PartiesClients.objects.filter(party=party)
+    else:        
+        party_clients_list = PartiesClients.objects.filter(party=party).filter(apply_status=apply_status)
+    
+    if party.invite_type == 'email':
+        for party_clinet in party_clients_list:
+            party_client_ajax = {
+                                  'name' : '',
+                                  'address':''
+                                }    
+            party_client_ajax['name'] = party_clinet.client.name
+            party_client_ajax['address'] = party_clinet.client.email
+            party_clients_list_ajax.append(party_client_ajax)
+    elif party.invite_type == 'phone':
+        for party_clinet in party_clients_list:
+            party_client_ajax = {
+                                 'name' : '',
+                                 'address':''
+                                }   
+            party_client_ajax['name'] = party_clinet.client.name
+            party_client_ajax['address'] = party_clinet.client.phone
+            party_clients_list_ajax.append(party_client_ajax)
+    else:
+        pass  
+              
+    returnjson = {
+                  'party_clients_list_ajax':party_clients_list_ajax                           
+                 }
+    returnjson = simplejson.dumps(returnjson)       
+    return HttpResponse(returnjson) 
