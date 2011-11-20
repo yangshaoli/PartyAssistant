e74bdea8d3ad49d2f1123a4a88782a30a905b253
liwenjian <liwenjian@cn-acg.com>
2011-11-20 15:34:06 +0800

    [fixed] #877

--- partyAssistant-backend/PartyAssistant/apps/messages/forms.py
+++ partyAssistant-backend/PartyAssistant/apps/messages/forms.py
@@ -26,7 +26,7 @@ class EmailInviteForm(forms.Form):
                 break
         
         if not validate_flag:
-            raise forms.ValidationError(u'email list error.')
+            raise forms.ValidationError(u'邮件地址格式错误')
         
         self.cleaned_data['client_email_list'] = ','.join(email_list)
         
@@ -50,7 +50,7 @@ class SMSInviteForm(forms.Form):
                 break
         
         if not validate_flag:
-            raise forms.ValidationError(u'phone list error.')
+            raise forms.ValidationError(u'电话号码格式错误')
         
         self.cleaned_data['client_phone_list'] = ','.join(phone_list)
         
