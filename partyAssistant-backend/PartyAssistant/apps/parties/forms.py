01bd111149ddaec1365d80728ba0541e208c2909
liwenjian <liwenjian@cn-acg.com>
2011-11-21 18:20:19 +0800

    split start_time to start_date and start_time

--- partyAssistant-backend/PartyAssistant/apps/parties/forms.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/forms.py
@@ -8,10 +8,11 @@ from django.forms.widgets import TextInput
 class CreatePartyForm(forms.ModelForm):
     class Meta:
         model = Party
-        fields = ('start_time', 'address', 'description', 'limit_count')
+        fields = ('start_date', 'start_time', 'address', 'description', 'limit_count')
         widgets = {
             'limit_count': TextInput(attrs={'maxlength':'3'}),
         }
+
 class InviteForm(forms.Form):
     addressee = forms.CharField(widget=forms.TextInput(), required=True)
     content = forms.CharField(widget=forms.TextInput(), required=True)   
