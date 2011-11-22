393ff3fed91ef61e28f22fd39ff268c35e9d3fc6
liwenjian <liwenjian@cn-acg.com>
2011-11-22 18:38:24 +0800

    [fixed] #876

--- partyAssistant-backend/PartyAssistant/apps/parties/urls.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns('apps.parties.views',
     url(r'^create_party/$', 'create_party', name='create_party'),
     url(r'^delete_party/(?P<party_id>\d+)/$', 'delete_party', name='delete_party'),
     url(r'^edit_party/(?P<party_id>\d+)/$', 'edit_party', name='edit_party'),
-    url(r'^copy_party/(?P<party_id>\d+)/$','copy_party', name='copy_party'),
     
     url(r'^(?P<party_id>\d+)/email_invite/$', 'email_invite',name='email_invite'),
     url(r'^(?P<party_id>\d+)/sms_invite/$', 'sms_invite',name='sms_invite'), 
