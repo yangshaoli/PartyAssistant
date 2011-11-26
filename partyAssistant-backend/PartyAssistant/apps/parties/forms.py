a0d29172d02fda96140c0196b72894928065d2c7
LiuXue <hljxuege@foxmail.com>
2011-11-26 19:42:07 +0800

    form

--- partyAssistant-backend/PartyAssistant/apps/parties/forms.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/forms.py
@@ -2,14 +2,18 @@
 
 from apps.parties.models import Party
 from django import forms
-from django.forms.widgets import TextInput
+from django.forms.widgets import TextInput, TimeInput, DateInput, Textarea
 
 class CreatePartyForm(forms.ModelForm):
     class Meta:
         model = Party
         fields = ('start_date', 'start_time', 'address', 'description', 'limit_count')
         widgets = {
-            'limit_count': TextInput(attrs={'maxlength':'3'}),
+            'limit_count': TextInput(attrs={'maxlength':'3', 'placeholder':u'无限制'}),
+            'start_time' : TimeInput(attrs={'placeholder':u'选填项,格式如8：00', 'class':'input-txt3 mys TimePker', 'autocomplete':'off'}),
+            'start_date' : DateInput(attrs={'placeholder':u'选填项,格式如2008-08-08', 'class':'input-txt3 mys', 'style':'width:150px'}),
+            'address'    : TextInput(attrs={'placeholder':u'可选填，主要作用是进行地图定位', 'style':'width=315px'}),
+            'description': Textarea( attrs={'placeholder':u'必填项', 'cols':'70', 'rows':'13'}),
         }
 
     def clean_limit_count(self):
