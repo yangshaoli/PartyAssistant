6163a02e6889c932d13f82ac07e22ace18b47f1b
LiuXue <hljxuege@foxmail.com>
2011-11-27 14:48:59 +0800

    add fake phone/email at apps/parties/views.py

--- partyAssistant-backend/PartyAssistant/apps/parties/views.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/views.py
@@ -461,8 +461,10 @@ def _public_enroll(request, party_id):
             phone = ''
             if form.cleaned_data['phone_or_email'].find('@') > 0:
                 email = form.cleaned_data['phone_or_email']
+                phone = form.cleaned_data['phone_or_email']+'@'#虚构phone
             else:
                 phone = form.cleaned_data['phone_or_email']
+                email = form.cleaned_data['phone_or_email']+'@'#虚构email
                     
             if Client.objects.filter(creator = creator).filter(email = email).count() == 0 \
                 and Client.objects.filter(creator = creator).filter(phone = phone).count() == 0:
