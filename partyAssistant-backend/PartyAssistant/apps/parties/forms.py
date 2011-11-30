87274b9b18ac230f73a02a707f5c68a69bdd1dad
LiuXue <hljxuege@foxmail.com>
2011-11-30 15:04:35 +0800

    add limit_count default num

--- partyAssistant-backend/PartyAssistant/apps/parties/forms.py
+++ partyAssistant-backend/PartyAssistant/apps/parties/forms.py
@@ -11,7 +11,7 @@ class CreatePartyForm(forms.ModelForm):
         model = Party
         fields = ('start_date', 'start_time', 'address', 'description', 'limit_count')
         widgets = {
-            'limit_count': TextInput(attrs={'maxlength':'3', 'placeholder':u'无限制'}),
+            'limit_count': TextInput(attrs={'maxlength':'3', 'placeholder':u'无限制', 'default':''}),
             'start_time' : TimeInput(attrs={'placeholder':u'选填项,格式如8：00', 'class':'input-txt3 mys TimePker', 'autocomplete':'off'}),
             'start_date' : DateInput(attrs={'placeholder':u'选填项,格式如2008-08-08', 'class':'input-txt3 mys', 'style':'width:150px'}),
             'address'    : TextInput(attrs={'placeholder':u'可选填，主要作用是进行地图定位', 'style':'width=315px'}),
