9b649f81e3dfd7cb6f68f6cd49718ad81d09dcda
LiuXue <hljxuege@foxmail.com>
2011-12-07 10:50:43 +0800

    delete charge

--- partyAssistant-backend/PartyAssistant/settings.py
+++ partyAssistant-backend/PartyAssistant/settings.py
@@ -130,9 +130,7 @@ INSTALLED_APPS = (
     'apps.parties',
     'apps.messages', 
     'apps.common', 
-    'apps.charge', 
 )
-
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
