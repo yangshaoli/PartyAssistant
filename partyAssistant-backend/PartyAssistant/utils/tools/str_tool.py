6f328c61a6702f5ebb1359f50c3f6c9d72072630
LiuXue <hljxuege@foxmail.com>
2011-12-16 16:25:50 +0800

    #1041 sms next_key

--- partyAssistant-backend/PartyAssistant/utils/tools/str_tool.py
+++ partyAssistant-backend/PartyAssistant/utils/tools/str_tool.py
@@ -7,9 +7,9 @@ Created on 2011-11-21
 def next_key(key):
     new_key = list(key)
     
-    choice = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    length = len(new_key)
-    for i in range(length, 0, -1):
+    choice = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    length = len(choice)
+    for i in range(len(new_key), 0, -1):
         c = new_key[i - 1]
         index = choice.index(c)
         index = index + 1
