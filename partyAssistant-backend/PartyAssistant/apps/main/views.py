709965ffddae8ce954d3933de8fbc59d334b2659
perchouli <jp.chenyang@gmail.com>
2011-12-28 10:32:28 +0800

    Change homepage template

--- partyAssistant-backend/PartyAssistant/apps/main/views.py
+++ partyAssistant-backend/PartyAssistant/apps/main/views.py
@@ -3,8 +3,10 @@ Created on 2011-12-6
 
 @author: liwenjian
 '''
+# -*- coding:utf-8 -*-
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 def home(request):
-    return redirect('list_party')
+    return TemplateResponse(request, 'home.html',)
