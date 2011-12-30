1ff9c898ca2496916ff109e50e05a39cce0fa7da
fengally <lichao0708@gmail.com>
2011-12-30 17:28:15 +0800

    fix the api func

--- partyAssistant-backend/PartyAssistant/apis/accounts/views.py
+++ partyAssistant-backend/PartyAssistant/apis/accounts/views.py
@@ -76,6 +76,13 @@ def accountRegist(request):
         if User.objects.filter(username = username):
             raise myException(ERROR_ACCOUNTREGIST_USER_EXIST)
         user = User.objects.create_user(username, '', password)
+        if user:
+            device_token = request.POST['device_token']
+            if device_token:
+                usertoken, created = UserIPhoneToken.objects.get_or_create(device_token = device_token, defaults = {'user' : user})
+                if usertoken.user != user:
+                    usertoken.user = user
+                    usertoken.save()
         return {'uid':user.id}
 
 
