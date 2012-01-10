730ba6e83cdaec75f4228014a4c77b0098f98233
fengally <lichao0708@gmail.com>
2012-01-10 20:35:46 +0800

    weibo bugs

--- partyAssistant-iphone/PartyAssistant/PartyAssistant/PostWeiboViewController.m
+++ partyAssistant-iphone/PartyAssistant/PartyAssistant/PostWeiboViewController.m
@@ -41,6 +41,7 @@
     self.navigationItem.rightBarButtonItem = _sendWeiboButton;
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"关闭" style:UIBarButtonItemStylePlain target:self action:@selector(cancelBtnTouched:)];
     NSString *default_text = WEIBO_DEFAULT_CONTENT;
+    NSLog(@"%@",partyObj.shortURL);
     default_text = [default_text stringByAppendingString:partyObj.shortURL];
     self.sendV = [[WBSendView alloc] initWithWeiboText:default_text withImage:nil andDelegate:self];
     sendV.delegate = self;
