9bf9c649b5ce2f54d26d3665d05c4e259bf73481
fengally <lichao0708@gmail.com>
2012-01-10 17:52:30 +0800

    apis

--- partyAssistant-backend/PartyAssistant/settings.py
+++ partyAssistant-backend/PartyAssistant/settings.py
@@ -15,11 +15,11 @@ ADMINS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'airenao', # Or path to database file if using sqlite3.
-        'USER': 'eventmgr', # Not used with sqlite3.
-        'PASSWORD': 'wo4liwenjian', # Not used with sqlite3.
-        'HOST': '192.168.2.100', # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '%s/dev.db'%PROJECT_ROOT, # Or path to database file if using sqlite3.
+        'USER': '', # Not used with sqlite3.
+        'PASSWORD': '', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '', # Set to empty string for default. Not used with sqlite3.
     }
 }
