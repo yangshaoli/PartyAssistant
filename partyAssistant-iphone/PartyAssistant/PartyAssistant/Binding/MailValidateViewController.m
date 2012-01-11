527dcf2fb33aa9e04b7cfca5a95cbdadb4d59391
Wang Jun <wjforstudy@localhost.(none)>
2012-01-11 17:17:22 +0800

    REV: modify binding UI.

---
+++ partyAssistant-iphone/PartyAssistant/PartyAssistant/Binding/MailValidateViewController.m
@@ -0,0 +1,69 @@
+//
+//  MailValidateViewController.m
+//  PartyAssistant
+//
+//  Created by Wang Jun on 1/11/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MailValidateViewController.h"
+
+@implementation MailValidateViewController
+@synthesize tableView = _tableView;
+@synthesize inputMailCell = _inputMailCell;
+@synthesize inputMailTextField = _inputMailTextField;
+@synthesize mailValidateCell = _mailValidateCell;
+@synthesize mailResendValidateCell = _mailResendValidateCell;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark _
+#pragma mark tableView delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ()
+    return nil;
+}
+@end
