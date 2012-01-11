519aec2dafd03740e72a5ab226c1e44544321d07
Shaoli Yang <yangshaoli@cn-acg.com>
2012-01-11 17:43:38 +0800

    ADD: add new class to project, to be finished

---
+++ partyAssistant-iphone/PartyAssistant/PartyAssistant/Services/UserInfoValidator.h
@@ -0,0 +1,13 @@
+//
+//  UserInfoValidator.h
+//  PartyAssistant
+//
+//  Created by Yang Shaoli on 12-1-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserInfoValidator : NSObject
+
+@end
---
+++ partyAssistant-iphone/PartyAssistant/PartyAssistant/Services/UserInfoValidator.m
@@ -0,0 +1,13 @@
+//
+//  UserInfoValidator.m
+//  PartyAssistant
+//
+//  Created by Yang Shaoli on 12-1-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserInfoValidator.h"
+
+@implementation UserInfoValidator
+
+@end
