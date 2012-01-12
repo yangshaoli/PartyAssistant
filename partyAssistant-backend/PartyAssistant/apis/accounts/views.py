4c554890e698cabba30febe2f0e03df9b1e389ac
fengally <lichao0708@gmail.com>
2012-01-12 15:11:56 +0800

    add binding apis

--- partyAssistant-backend/PartyAssistant/apis/accounts/views.py
+++ partyAssistant-backend/PartyAssistant/apis/accounts/views.py
@@ -17,7 +17,9 @@ from utils.structs.my_exception import myException
 from utils.tools.phone_num_tool import regPhoneNum
 from utils.tools.phone_key_tool import generate_phone_code
 from utils.tools.apis_json_response_tool import apis_json_response_decorator
+
 import re
+import hashlib
 
 from ERROR_MSG_SETTINGS import *
 from ERROR_STATUS_SETTINGS import *
@@ -120,12 +122,22 @@ def getBadgeNum(request):
 @apis_json_response_decorator
 @commit_on_success
 def profilePage(request, uid):
-    user = User.objects.get(pk = id)
+    user = User.objects.get(pk = id).select_related('userprofile')
     if request.method == 'POST':
-#        nickname = 
-        pass
+        nickname = request.POST['nickname']
+        if len(nickname) > 16:
+            raise myException(ERROR_PROFILEPAGE_LONG_NAME)
+        user.userprofile.true_name = nickname
+        user.userprofile.save()
     else:
-        pass
+        return {
+                'nickname':user.userprofile.true_name,
+                'remaining_sms_count':user.userprofile.available_sms_count,
+                'email':user.userprofile.email,
+                'email_binding_status':user.userprofile.email_binding_status,
+                'phone':user.userprofile.phone,
+                'phone_binding_status':user.userprofile.phone_binding_status,
+                }
 
 @csrf_exempt
 @apis_json_response_decorator
@@ -190,7 +202,10 @@ def bindContact(request, type):
             user = User.objects.get(pk = uid).select_related('userprofile')
         except Exception:
             raise myException(ERROR_BINDING_NO_USER)
-        userkey = generate_phone_code()
+        if type == 'email':
+            userkey = hashlib.md5(value).hexdigest()
+        else:
+            userkey = generate_phone_code()
         data = {"latest_status":{
                                  'email':user.userprofile.email,
                                  'email_binding_status':user.userprofile.email_binding_status,
@@ -209,20 +224,18 @@ def bindContact(request, type):
             else:
                 raise myException(ERROR_BINDING_BY_PHONE_DIFFERENT_BINDED, status = ERROR_STATUS_DIFFERENT_BINDED, data = data)
         if type == 'email' and UserProfile.objects.filter(email = value, email_binding_status = 'bind').exclude(user = user).count() != 0:
-            raise myException(ERROR_STATUS_BINDING_BY_EMAIL_HAS_BINDED_BY_OTHER, status = ERROR_HAS_BINDED_BY_OTHER, data = data)
+            raise myException(ERROR_HAS_BINDED_BY_OTHER, status = ERROR_STATUS_BINDING_BY_EMAIL_HAS_BINDED_BY_OTHER , data = data)
         elif  type == 'phone' and UserProfile.objects.filter(phone = value, phone_binding_status = 'bind').exclude(user = user).count() != 0:
-            raise myException(ERROR_STATUS_BINDING_BY_PHONE_HAS_BINDED_BY_OTHER, status = ERROR_HAS_BINDED_BY_OTHER, data = data)
+            raise myException(ERROR_HAS_BINDED_BY_OTHER, status = ERROR_STATUS_BINDING_BY_PHONE_HAS_BINDED_BY_OTHER , data = data)
 
         binding_temp, created = UserBindingTemp.objects.get_or_create(user = request.user, binding_type = type, defaults = {"binding_address":value, "key":userkey})
         if not created:
             binding_temp.binding_addres = value
             binding_temp.key = userkey
             binding_temp.save()
-        phone = request.user.get_profile().phone
-           
         profile = request.user.get_profile()
         profile.phone = value
-        profile.phone_binding_status = 'waiteunbind'
+        profile.phone_binding_status = 'waitingbind'
         profile.save()
         data = {"latest_status":{
                                  'email':user.userprofile.email,
@@ -239,10 +252,104 @@ def bindContact(request, type):
 @commit_on_success
 @apis_json_response_decorator
 def unbindContact(request, type):
-    pass
+    if request.method == 'POST':
+        value = request.POST['value']
+        uid = request.POST['uid']
+        try:
+            user = User.objects.get(pk = uid).select_related('userprofile')
+        except Exception:
+            raise myException(ERROR_BINDING_NO_USER)
+        if type == 'email':
+            userkey = hashlib.md5(value).hexdigest()
+        else:
+            userkey = generate_phone_code()
+        data = {"latest_status":{
+                                 'email':user.userprofile.email,
+                                 'email_binding_status':user.userprofile.email_binding_status,
+                                 'phone':user.userprofile.phone,
+                                 'phone_binding_status':user.userprofile.phone_binding_status,
+                                 }
+                }
+        # 确定当前用户状态是已绑定
+        if (type == 'email' and user.userprofile.email_binding_status != 'bind')\
+            or (type == 'phone' and user.userprofile.phone_binding_status != 'bind'):
+            raise myException(ERROR_UNBINDING, status = ERROR_STATUS_DIFFERENT_UNBINDED, data = data)
+        # 确定app端的绑定信息是用户最新的绑定信息
+        if (type == 'email' and user.userprofile.email != value)or(type == 'phone' and user.userprofile.phone != value):
+            raise myException(ERROR_UNBINDING, status = ERROR_STATUS_DIFFERENT_UNBINDED, data = data)
+        # 发送验证码
+        binding_temp, created = UserBindingTemp.objects.get_or_create(user = request.user, binding_type = type, defaults = {"binding_address":value, "key":userkey})
+        if not created:
+            binding_temp.binding_addres = value
+            binding_temp.key = userkey
+            binding_temp.save()
+        data = {"latest_status":{
+                                 'email':user.userprofile.email,
+                                 'email_binding_status':user.userprofile.email_binding_status,
+                                 'phone':user.userprofile.phone,
+                                 'phone_binding_status':user.userprofile.phone_binding_status,
+                                 }
+                }
+        return data
 
 @csrf_exempt
 @commit_on_success
 @apis_json_response_decorator
 def verifyContact(request, type):
-    pass
+    if request.method == 'POST':
+        value = request.POST['value']
+        uid = request.POST['uid']
+        userkey = request.POST['userkey']
+        try:
+            user = User.objects.get(pk = uid).select_related('userprofile')
+        except Exception:
+            raise myException(ERROR_BINDING_NO_USER)
+        data = {"latest_status":{
+                                 'email':user.userprofile.email,
+                                 'email_binding_status':user.userprofile.email_binding_status,
+                                 'phone':user.userprofile.phone,
+                                 'phone_binding_status':user.userprofile.phone_binding_status,
+                                 }
+                }
+        # 确定当前用户状态是等待绑定状态或者是已绑定状态
+        if (type == 'email' and user.userprofile.email_binding_status == 'unbind')\
+            or (type == 'phone' and user.userprofile.phone_binding_status == 'unbind'):
+            raise myException(ERROR_VERIFY, status = ERROR_STATUS_INVALID_VERIFIER, data = data)
+        # 确定app端的绑定信息是用户最新的绑定信息
+        if (type == 'email' and user.userprofile.email != value)or(type == 'phone' and user.userprofile.phone != value):
+            raise myException(ERROR_VERIFY, status = ERROR_STATUS_INVALID_VERIFIER, data = data)
+        # 确定app端的绑定邮箱/手机号未被别的用户使用
+        if type == 'email' and UserProfile.objects.filter(email = value, email_binding_status = 'bind').exclude(user = user).count() != 0:
+            raise myException(ERROR_VERIFYING_BY_EMAIL_HAS_BINDED_BY_OTHER, status = ERROR_HAS_BINDED_BY_OTHER, data = data)
+        elif  type == 'phone' and UserProfile.objects.filter(phone = value, phone_binding_status = 'bind').exclude(user = user).count() != 0:
+            raise myException(ERROR_VERIFYING_BY_PHONE_HAS_BINDED_BY_OTHER, status = ERROR_HAS_BINDED_BY_OTHER, data = data)
+        
+        #开始解绑
+        binding_temp = UserBindingTemp.objects.filter(user = request.user, binding_type = type, binding_address = value, key = userkey)
+        if not binding_temp:
+            raise myException(ERROR_VERIFYING_WRONG_VERIFIER)
+        if type == 'email':
+            if user.userprofile.email_binding_status == 'waitingbind':
+                user.userprofile.email_binding_status = 'bind'
+                user.userprofile.save()
+            elif user.userprofile.email_binding_status == 'bind':
+                user.userprofile.email_binding_status = 'unbind'
+                user.userprofile.email = ''
+                user.userprofile.save()
+        elif type == 'phone':
+            if user.userprofile.phone_binding_status == 'waitingbind':
+                user.userprofile.phone_binding_status = 'bind'
+                user.userprofile.save()
+            elif user.userprofile.phone_binding_status == 'bind':
+                user.userprofile.phone_binding_status = 'unbind'
+                user.userprofile.phone = ''
+                user.userprofile.save()
+        data = {"latest_status":{
+                                 'email':user.userprofile.email,
+                                 'email_binding_status':user.userprofile.email_binding_status,
+                                 'phone':user.userprofile.phone,
+                                 'phone_binding_status':user.userprofile.phone_binding_status,
+                                 }
+                }
+        return data
+    
