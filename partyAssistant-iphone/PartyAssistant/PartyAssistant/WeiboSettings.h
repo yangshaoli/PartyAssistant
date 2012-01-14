8e22d046cd38a4eea246834eb676c01d0a3fe1da
Shaoli Yang <yangshaoli@cn-acg.com>
2012-01-14 20:07:32 +0800

    FIX:fix bug,ticket 1217

--- partyAssistant-iphone/PartyAssistant/PartyAssistant/WeiboSettings.h
+++ partyAssistant-iphone/PartyAssistant/PartyAssistant/WeiboSettings.h
@@ -10,6 +10,6 @@
 #define WEIBOPRIVATEAPPKEY @"999433557"
 #define WEIBOPRIVATEAPPSECRETE @"8ebb477102459b3387da43686b21c963"
 
-#define WEIBO_DEFAULT_URL [NSString stringWithFormat:@"%@/parties/party_id/enroll/", DOMAIN_NAME]
+#define WEIBO_DEFAULT_URL [NSString stringWithFormat:@"%@/parties/party_id/enroll/ ", DOMAIN_NAME]
 #define WEIBO_DEFAULT_CONTENT [NSString stringWithFormat:@"我使用@我们爱热闹 发布了一个活动！大家快来报名："]
 
