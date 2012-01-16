bb099477240e7f21cc9507644efa666612c6e914
fengally <lichao0708@gmail.com>
2012-01-16 19:37:26 +0800

    fix bug

--- partyAssistant-backend/PartyAssistant/apis/parties/views.py
+++ partyAssistant-backend/PartyAssistant/apis/parties/views.py
@@ -528,6 +528,6 @@ def resendMsg(request):
         
         return {
                 'partyId':party.id,
-                'applyURL':DOMAIN_NAME + reverse('enroll', args = [party.id]),
+                'applyURL':transfer_to_shortlink(DOMAIN_NAME + reverse('enroll', args = [party.id])),
                 'sms_count_remaining':user.userprofile.available_sms_count,
                 }
