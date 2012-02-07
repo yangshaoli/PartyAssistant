69a7db11db8e00bac3cbcd0a1ab9a69378123128
unknown <hanlay@hanlay-PC.(none)>
2012-02-07 15:37:53 +0800

    modify layout and change the words on the button to Chinese

--- partAssistant-android/ImeetingClient/src/com/aragoncg/apps/airenao/activity/PreviewActivity.java
+++ partAssistant-android/ImeetingClient/src/com/aragoncg/apps/airenao/activity/PreviewActivity.java
@@ -6,22 +6,30 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aragoncg.apps.airenao.R;
+import com.aragoncg.apps.airenao.constans.Constants;
 import com.aragoncg.apps.airenao.model.MyPerson;
+import com.aragoncg.apps.airenao.utills.AirenaoUtills;
 
 public class PreviewActivity extends Activity {
 	private Intent personIntent;
 	private List<MyPerson> list;
 	private boolean flag = false;
 	private int count;
+	private LinearLayout userLayout;
+	private TextView userTitle;
+	private String userName = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,15 @@ public class PreviewActivity extends Activity {
 				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
 		setContentView(R.layout.activity_preview);
+		userLayout = (LinearLayout) findViewById(R.id.userChange);
+		userTitle = (TextView)findViewById(R.id.userTitle);
+		SharedPreferences mySharedPreferences = AirenaoUtills
+		.getMySharedPreferences(this);
+		userName = mySharedPreferences.getString(Constants.AIRENAO_USER_NAME,
+		null);
+		if(!"".equals(userName) && userName != null){
+			userTitle.setText(userName);
+		}
 		flag = false;
 		ListView listView = (ListView) findViewById(R.id.listviewId);
 		Button button = (Button) findViewById(R.id.btn_finish);
